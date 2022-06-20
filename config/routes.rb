a0eae6d2dd25ab67c3e2c793a7d03e0c7b152025
vishnu <vishnu.kv@kreatio.com>
2022-06-20 15:44:21 +0530

    remove old signup and add new sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles
-  resources :users
-  #resources :articles
+  get 'signup', to: "users#new"
+  resources :users, except: ['new']
+  # post 'users', to: "users#create"
 end
