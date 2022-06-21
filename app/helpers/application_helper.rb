4352dde21352093a543d62ea6a7d0c7be562040c
vishnu <vishnu.kv@kreatio.com>
2022-06-21 12:54:41 +0530

    add user profile using gravatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def gravatar_for(user, size = 80)
+    email_address = user.email
+    hash = Digest::MD5.hexdigest(email_address)
+    gravatar_image = "https://www.gravatar.com/avatar/#{hash}?s=#{size}"
+    image_tag(gravatar_image,alt: user.username)
+  end
 end
